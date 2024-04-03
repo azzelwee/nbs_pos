b1ff5a453ef5f6aaf152e4783e4105690bab3492
Azzelswee <azzelswee@gmail.com>
2024-04-03 11:04:31 +0800

    Push

--- index.php
+++ index.php
@@ -24,108 +24,128 @@ $con = connection();
     </div>
 
     <div class= "gray">
-        <div class ="trx-number">
-            <?php
-            if(isset($_SESSION['UserLogin'])){
-                echo "Trx.: ".$_SESSION['Trx'];
-            } else {
-                echo "Trx: None";
-            }        
-            ?>
-        </div>
-
-        <div class="clerk">
-            <?php
-            if(isset($_SESSION['UserLogin'])){
-                echo "Clerk: ".$_SESSION['Name'];
-            } else {
-                echo "Guest";
-            }        
-            ?>
-        </div>
-
-        <div class="str">
-            <?php
-            if(isset($_SESSION['UserLogin'])){
-                echo "Str No.: 1000";
-            } else {
-                echo "Str No.:";
-            }        
-            ?>
-        </div>
-
-        <div class="reg">
-            <?php
-            if(isset($_SESSION['UserLogin'])){
-                echo "Reg No.: 0001";
-            } else {
-                echo "Reg No.:";
-            }        
-            ?>
-        </div>
-
-    </div>
-    <div class="input-container">
-        <div class="input-container-left">
-            <div class= "yellow-index">
-                <label>Scan or Enter UPC: </label>
-                <input type="scan" name="scan" id="scan">
+            <div class ="trx">
+                <?php
+                if(isset($_SESSION['UserLogin'])){
+                    echo "Trx.: ".$_SESSION['Trx'];
+                } else {
+                    echo "Trx.:";
+                }        
+                ?>
             </div>
 
-            <div class="test">
-                <div class="box1">
-                    <h2>F11</h2>
+            <div class="clerk">
+                <?php
+                if(isset($_SESSION['UserLogin'])){
+                    echo "Clerk: ".$_SESSION['Name'];
+                } else {
+                    echo "Guest";
+                }        
+                ?>
+            </div>
+
+            <div class="str">
+                <?php
+                if(isset($_SESSION['UserLogin'])){
+                    echo "Str No.: 1000";
+                } else {
+                    echo "Str No.:";
+                }        
+                ?>
+            </div>
+
+            <div class="reg">
+                <?php
+                if(isset($_SESSION['UserLogin'])){
+                    echo "Reg No.: 0001";
+                } else {
+                    echo "Reg No.:";
+                }        
+                ?>
+            </div>
+
+    </div>
+    <div class="main-cont">
+        <div class="input-container">
+            <div class="input-container-left">
+                <div class= "yellow-index">
+                    <label>Scan or Enter UPC: </label>
+                    <input type="scan" name="scan" id="scan">
                 </div>
-                <div class="box2">
-                    <h2>F12</h2>
-                </div>
-                <div class="box3">
-                    <h2>CSA</br>
+
+                <div class="button-container">
+                    <div class="button1">
+                        F11
+                    </div>
+                    <div class="button2">
+                        F12
+                    </div>
+                    <div class="button3">
+                        CSA</br>
                         ON/OFF</br>
-                        F10</h2>
-                </div>
-                <div class="box4">
-                    <h2>Lookup</br>
+                        F10
+                    </div>
+                    <div class="button4">
+                        Lookup</br>
                         F2</h2>
+                    </div>
                 </div>
             </div>
-            <div class="gray-below">
-                test
-            </div>
-        </div>
-
-        <div class="input-container-right">
-            <div class="black-index">
-                <div class="subtotal">
-                    <p>Subtotal: </p>
-                </div>
-
-                <div class="quantity">
-                    <p>Quantity: </p>
-                </div>
-
-                <div class="unit-price">
-                    <p>Unit Price: </p>
-                </div>
-            </div>
-
-
-        </div>
             
-        <div class="input-container-rightmost">
-            <div class="b1">Container 1</div>
-            <div class="b2">Container 2</div>
-            <div class="b3">Container 3</div>
-            <div class="b4">Container 4</div>
-            <div class="b5">Container 5</div>
-            <div class="b6">Container 6</div>
-            <div class="b7">Container 7</div>
+
+            <div class="input-container-right">
+                <div class="black-index">
+                    <div class="subtotal">
+                        <p>Subtotal: </p>
+                    </div>
+
+                    <div class="quantity">
+                        <p>Quantity: </p>
+                    </div>
+
+                    <div class="unit-price">
+                        <p>Unit Price: </p>
+                    </div>
+                </div>
+                
+            </div>
+            
+        
+            <div class="input-container-rightmost">
+                <div class="gray-index">
+                    <div class="container1">
+                    test
+                    </div>
+                    <div class="container2">
+                    test
+                    </div>
+                    <div class="container3">
+                    test
+                    </div>
+                    <div class="container4">
+                    test
+                    </div>
+                    <div class="container5">
+                    test
+                    </div>
+                    <div class="container6">
+                    test
+                    </div>
+                    <div class="container7">
+                    test
+                    </div>
+                </div>
+            </div>
+            
         </div>
+        
+    </div>
+    
     </div>
 
-    
-    <div class="green">
-    </div>
+
+    <!-- <div class="green">
+    </div> -->
 
     <!-- <script src="js/main.js"></script> -->
 
